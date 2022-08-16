6fe1514b36b80c32fe27e4e31b73837373941d03
anna <slukin1@yandex.ru>
2022-08-16 20:58:46 +0700

    tesk29

---
+++ Task29/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 29
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный 0 и 1 в случайном порядке  
+// [1,0,1,1,0,1,0,0]
+
+
+Console.Write("Введите число элементов массива: ");
+int number = int.Parse(Console.ReadLine());
+if (number == 0)
+{
+    Console.Write("Ввели некорректное значение");
+    return;
+}
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(1, 100);
+    }
+    return array;
+}
+int[] result = GetArray(number);
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]},");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+}
+PrintArray(result);
+
